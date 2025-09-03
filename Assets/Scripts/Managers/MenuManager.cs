73b3162a32049674100d13653d590c7afd1f4c80
Kyoji-Aeris <kyojiaeris@gmail.com>
2025-09-03 23:10:30 +0800

    UI & Main Menu
    -Added a scene: Main Menu
    -Added UI: Character & Sanity Meter(Prototype)
    -Added a pause menu.

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject pausePanel;
+    public void TogglePausePanel()
+    {
+        Time.timeScale = 0f;
+        pausePanel.SetActive(true);
+    }
+
+    public void ToggleUnpausePanel()
+    {
+        Time.timeScale = 1f;
+        pausePanel.SetActive(false);
+    }
+    public void SwitchScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private void Start()
+    {
+        Time.timeScale = 1f;
+    }
+}
